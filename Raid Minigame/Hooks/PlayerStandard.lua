1be463f37fa09b6fc0800f8e0b0e5453ad365395
Luffyyy <danielandro11@gmail.com>
2020-06-08 19:38:21 +0300

    Fixed a crash in hell's island

--- Raid Minigame/Hooks/PlayerStandard.lua
+++ Raid Minigame/Hooks/PlayerStandard.lua
@@ -16,12 +16,15 @@ end
 
 Hooks:PostHook(PlayerStandard, "_start_action_interact", "RaidMinigameCheckInteraction", function(self, t, input, timer, object)
 	if alive(object) then
-        local tweak = tweak_data.interaction[object:interaction().tweak_data]
-		if tweak and ((tweak.is_lockpicking and RaidMinigame.Options:GetValue("ModifyLockpicks")) or tweak.special_interaction == "raid") then
-			game_state_machine:change_state_by_name("ingame_special_interaction", {object = object, type = tweak.special_interaction or "raid"})
-			self._interact_expire_t = nil
-			managers.hud:hide_interaction_bar()
-			object:interaction():_post_event(self._unit, "sound_interupt")
+		local name = object:interaction().tweak_data
+		local tweak = tweak_data.interaction[name]
+		if name ~= "open_door_with_keys" then -- Crashes & is not really lockpicking in the same sense.
+			if tweak and ((tweak.is_lockpicking and RaidMinigame.Options:GetValue("ModifyLockpicks")) or tweak.special_interaction == "raid") then
+				game_state_machine:change_state_by_name("ingame_special_interaction", {object = object, type = tweak.special_interaction or "raid"})
+				self._interact_expire_t = nil
+				managers.hud:hide_interaction_bar()
+				object:interaction():_post_event(self._unit, "sound_interupt")
+			end
 		end
 	end
 end)
